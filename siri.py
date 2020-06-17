aca12d6dd2930c2d84203296d39e14debe6c1d78
VoHoa-HCMUS <vohoa1468@gmail.com>
2020-06-17 16:11:34 +0700

    exception handle

--- siri.py
+++ siri.py
@@ -2,24 +2,33 @@ import pyttsx3
 from datetime import date
 import wikipedia
 
-#pip install pipwin32
-#pipwin install pyaudio
-#pip install pyttsx3 
+# pip install pipwin32
+# pipwin install pyaudio
+# pip install pyttsx3
+# pip install wikipedia
+
 
 def findWiki():
-    username=input("Enter your name: ")
-    text="Hello " +username+ ",input infomation you need to find, i will do it for you"
+    username = input("Enter your name: ")
+    text = "Hello " + username + " , input infomation you need to find, i will do it for you"
     engine.say(text)
     engine.runAndWait()
     while True:
-        find=input("Input infomation: ")
+        find = input("Input infomation: ")
         print("Robot:....")
-        content=wikipedia.summary(find,sentences=1)
-        print("Find it!!!")
-        print(content)
-        engine.say(content)
-        engine.runAndWait()
-#main
+        try:
+            content = wikipedia.summary(find, sentences=1)
+            print("Find it!!!")
+            print(content)
+            engine.say(content)
+            engine.runAndWait()
+        except:
+            print("Can't find!!!")
+            engine.say("Can't find, try another one")
+            engine.runAndWait()
+
+
+# main
 engine = pyttsx3.init()
 engine.setProperty('rate', 180)
 voices = engine.getProperty('voices')
